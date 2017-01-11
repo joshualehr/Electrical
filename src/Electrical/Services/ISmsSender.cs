15bdf8c02f06fa62fb4223eb1eda4f4837f7d519
joshualehr <joshuadlehr@hotmail.com>
2017-01-11 15:32:50 -0500

    Add project files.

---
+++ src/Electrical/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electrical.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
